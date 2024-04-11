261d6427c9eee5086b4e1f0abe13c5e591e04bb5
FallenLogic <68028253+FallenLogic@users.noreply.github.com>
2024-04-11 00:12:29 -0400

    Added height adjustment

--- capstone.py
+++ capstone.py
@@ -35,23 +35,39 @@ map_settings = MapSettings()
 
 default_padding = 5
 
+
 class ImageButton(ttk.Button):
     # Adapted from: https://stackoverflow.com/questions/69167947/how-to-change-tkinter-button-image-on-click
-    def __init__(self, master, image1, image2, *args, **kwargs):
-        self.unclickedImage = tk.PhotoImage(file=image1)
-        self.clickedImage = tk.PhotoImage(file=image2)
-        super().__init__(master, *args, image=self.unclickedImage, **kwargs)
-        self.toggleState = 1
+    def __init__(self, master, image1, image2, image3, image4, image5, image6, *args, **kwargs):
+        self.voidImage = tk.PhotoImage(file=image1)
+        self.height1Image = tk.PhotoImage(file=image2)
+        self.height2Image = tk.PhotoImage(file=image3)
+        self.height3Image = tk.PhotoImage(file=image4)
+        self.height4Image = tk.PhotoImage(file=image5)
+        self.height5Image = tk.PhotoImage(file=image6)
+
+        super().__init__(master, *args, image=self.voidImage, **kwargs)
+        self.toggleState = 0
         self.bind("<Button-1>", self.click_function)
 
     def click_function(self, event=None):
         if self.cget("state") != "disabled":  # Ignore click if button is disabled
-            self.toggleState *= -1
-            if self.toggleState == -1:
-                self.config(image=self.clickedImage)
+            if self.toggleState >= 5:
+                self.toggleState = 0
             else:
-                self.config(image=self.unclickedImage)
-
+                self.toggleState += 1
+            if self.toggleState == 0:
+                self.config(image=self.voidImage)
+            if self.toggleState == 1:
+                self.config(image=self.height1Image)
+            if self.toggleState == 2:
+                self.config(image=self.height2Image)
+            if self.toggleState == 3:
+                self.config(image=self.height3Image)
+            if self.toggleState == 4:
+                self.config(image=self.height4Image)
+            if self.toggleState == 5:
+                self.config(image=self.height5Image)
 
 def load_last_prompt():
     with open('prompt_log.txt', 'r') as fin:
@@ -61,25 +77,15 @@ def load_last_prompt():
             input_text.insert(0, last_prompt[:-1])  # [:-1 cuts \n out]
 
 
-def load_props(filename):
-    with open(filename) as fin:
-        for line in fin:
-            if line.startswith("("):
-                overall_data = []
-                words = set(line[1:-2].split(","))
-            else:
-                prop_data = line.split(":")
-                prop_name = prop_data[0]
-                conditional_probability = float(prop_data[-1])
-                data_set = [prop_name, conditional_probability]
-                overall_data.append(data_set)
-                print(overall_data)
+class Timing:
+    average_time = 0
+    average_time_count = 0
 
 
 if __name__ == "__main__":
     prop_path = "prop_sample_data.txt"
 
-    load_props(prop_path)
+    # load_props(prop_path)
 
     # table_relation = Relation(("table", "desk", "stand"), 0.8, "props_c17\\furnituretable003a.mdl")
     # table_relation.save_relation()
@@ -92,7 +98,7 @@ if __name__ == "__main__":
 
     window = tk.Tk()
     window.title("Capstone")
-    window.geometry("800x500")
+    window.geometry("800x640")
 
     notebook = ttk.Notebook(window)
 
@@ -110,12 +116,12 @@ if __name__ == "__main__":
 
     temp_spinbox = ttk.Spinbox(frame, to=5, width=4)
     temp_spinbox.grid(column=1, row=2, sticky=tk.W, padx=default_padding, pady=default_padding)
-    temp_spinbox.insert(0,'0')
+    temp_spinbox.insert(0, '0')
 
     grid_var = tk.IntVar()
-    grid_size_spinbox = ttk.Spinbox(frame, to=8, width=4, textvariable=grid_var)
+    grid_size_spinbox = ttk.Spinbox(frame, to=13, width=4, textvariable=grid_var)
     grid_size_spinbox.grid(column=1, row=2, sticky=tk.N, padx=default_padding, pady=default_padding)
-    grid_size_spinbox.set(8)
+    grid_size_spinbox.set(10)
 
     grid_frame = tk.Frame(frame)
     grid_frame.grid(column=1, row=4, sticky=tk.N)
@@ -127,40 +133,39 @@ if __name__ == "__main__":
 
     theme_combobox.grid(column=2, row=2, sticky=tk.W, padx=default_padding, pady=default_padding)
 
-    grid_size = 8
-
     button_grid = []
 
 
-    def switch_tile_value(i, j):  # using numbers as booleans ("truthy" values in python)
-        if true_grid[i][j] == 1:
-            true_grid[i][j] = 0
-        else:
-            true_grid[i][j] = 1
-
-
-    map_grid_tile_size = MAX_SUPPORTED_SIZE / 8 #TODO: data from spinbox
-
-
     def add_grid():
-
-        geo_button['state'] = 'disabled'
-
-        for i in range(grid_size):
-            row = []
-            for j in range(grid_size):
-                command = partial(switch_tile_value, i, j)
-                f1 = ImageButton(grid_frame, image1="res/empty2.png", image2="res/handpaintedwall2.png",
+        geo_button['state'] = 'enabled'
+        gui_grid_size = grid_var.get()
+        for gui_i in range(gui_grid_size):
+            gui_row = []
+            for gui_j in range(gui_grid_size):
+                command = partial(update_tile_value, gui_i, gui_j)
+                f1 = ImageButton(grid_frame, image1="res/empty2.png", image2="res/h1.png",
+                                 image3="res/h2.png", image4="res/h3.png", image5="res/h4.png", image6="res/h5.png",
                                  command=command)
-                f1.grid(column=1 + i, row=4 + j)
-                row.append(f1)
-            button_grid.append(row)
+                f1.grid(column=1 + gui_i, row=4 + gui_j)
+                gui_row.append(f1)
+            button_grid.append(gui_row)
 
 
     geo_button = ttk.Button(frame, text="Do Geometry Painting", command=add_grid)
     geo_button.grid(column=1, row=3, sticky=tk.N, padx=default_padding, pady=default_padding)
 
+
+    def update_tile_value(i, j):  # using numbers as booleans later ("truthy" values in python), kind of hacky
+        if true_grid[i][j] == 5:  # 1 = floor, 2 = grid tile height, 3 = 2*grid tile height, 4 = 3*grid tile height, 5=4*grid_tile_height
+            true_grid[i][j] = 0
+        else:
+            true_grid[i][j] += 1
+        print(true_grid[i][j])
+
+
     def generate():
+        gen_grid_size = grid_var.get()
+        map_grid_tile_size = MAX_SUPPORTED_SIZE / gen_grid_size
         start = timer()
         if theme_combobox.get() == 'Urban':
             map_settings.floor_material = "concrete/concretefloor033k_c17"
@@ -177,7 +182,7 @@ if __name__ == "__main__":
                 int(prevalidation_temperature) < 0 or int(prevalidation_temperature) > 5):
             messagebox.showwarning(title="Warning", message="Temperature not set or invalid temperature. Using default")
             temp_spinbox.set(0)
-        if input_text.get() == '': # tkinter text input usually handles other edge cases (typing \/, os.open, etc)
+        if input_text.get() == '':  # tkinter text input usually handles other edge cases (typing \/, os.open, etc)
             messagebox.showwarning(title="Warning", message="No prompt input or invalid input. Using random.")
 
             noun_index = randint(0, len(nouns) - 1)
@@ -208,64 +213,57 @@ if __name__ == "__main__":
 
         prop_list = []
         geometry_list = []
-        #size = 4096
-        height = 2
-        # default values
-        left, right, below, above = 0, 0, 0, 0
-        for i in range(grid_size):
-            for j in range(grid_size):
-                if true_grid[i][j] == 1:
-                    if j + 1 < grid_size:
-                        right = true_grid[i][j + 1]
-                    if i > 0:
-                        left = true_grid[i][j - 1]
-                    if i + 1 < grid_size:
-                        below = true_grid[i + 1][j]
-                    if i > 0:
-                        above = true_grid[i - 1][j]
-                    # checks if on the outside
-                    if not (left and right and below and above):
-                        height = map_grid_tile_size
-                    else:
-                        height = 2
+
+        for i in range(gen_grid_size):
+            for j in range(gen_grid_size):
+                if true_grid[i][j] > 0:
                     # TODO: update to match chunk-based generation
+                    if true_grid[i][j] == 1:
+                        height = 2
+                    else:
+                        height = true_grid[i][j] * map_grid_tile_size
                     if height == 2:
                         prop_x = (((i + j * (uniform(0, temperature_val)) / 10) * map_grid_tile_size) - (
                                 MAX_SUPPORTED_SIZE / 2) + (map_grid_tile_size / 2))
                         prop_y = (((j + i * (uniform(0, temperature_val)) / 10) * map_grid_tile_size) - (
-                                                          MAX_SUPPORTED_SIZE / 2) + (map_grid_tile_size / 2))
-                        prop_z = 22 # TODO: fix magic number
-                        test_prop = geometry.Prop(prop_x,
-                                                  prop_y,
-                                                  prop_z, True, False, False,
-                                                  prop_name)
-                        prop_list.append(test_prop)
+                                MAX_SUPPORTED_SIZE / 2) + (map_grid_tile_size / 2))
+                        prop_z = 22  # TODO: fix magic number
+                        for prop_num in range(10):  # TODO: fix this magic number too
+                            test_prop = geometry.Prop(prop_x + prop_num * 48,
+                                                      prop_y + randint(0, temperature_val * prop_num * 32),
+                                                      prop_z, True, False, False,
+                                                      prop_name)
+                            prop_list.append(test_prop)
 
-                    test_cube = geometry.Primitive("BLOCK", (i * map_grid_tile_size - MAX_SUPPORTED_SIZE / 2),
-                                                   (j * map_grid_tile_size - MAX_SUPPORTED_SIZE / 2), 0, map_grid_tile_size, map_grid_tile_size,
+                    test_primitive = geometry.Primitive("BLOCK", (i * map_grid_tile_size - MAX_SUPPORTED_SIZE / 2),
+                                                   (j * map_grid_tile_size - MAX_SUPPORTED_SIZE / 2), 0,
+                                                   map_grid_tile_size, map_grid_tile_size,
                                                    height, i, map_settings.floor_material, map_settings.wall_material)
+                    geometry_list.append(test_primitive)
 
-                    geometry_list.append(test_cube)
-
-        if should_write_primitives:
+        if enable_prims_button.instate(['selected']) or enable_prims_button.instate(['alternate']):
             for brush in geometry_list:
                 brush.save_to_file(out_file)
-        if should_write_prefabs:
-            file_utils.append_file_to_file("prefabs/big_skybox.vmf", out_file) #TODO: update to scale
-            if input_str.__contains__("house"):
-                file_utils.append_file_to_file("prefabs/prefab_house_1.vmf",out_file)
-        if should_write_props:
-            with open(out_file, 'a') as fout:
-                fout.write("}")
+        if enable_prefabs_button.instate(['selected']) or enable_prefabs_button.instate(['alternate']):
+            if should_write_prefabs:
+                file_utils.append_file_to_file("prefabs/big_skybox.vmf", out_file)  # TODO: update to scale
+                if input_str.__contains__("house"):  # TODO: generalize
+                    file_utils.append_file_to_file("prefabs/prefab_house_1.vmf", out_file)
+        with open(out_file, 'a') as fout:
+            fout.write("}")
+        if enable_props_button.instate(['selected']) or enable_props_button.instate(['alternate']):
             for prop in prop_list:
                 prop.save_to_file(out_file)
         end = timer()
+        Timing.average_time_count += 1
+        Timing.average_time += (end - start)
 
-        overall_time = end-start
-        map_settings.ttc = overall_time
-        print("Time to completion: " + str(overall_time)) #TODO: integrate into GUI
-        calculated_time_label['text'] = str(overall_time) + "s"
+        print("Time to completion: " + str(Timing.average_time / Timing.average_time_count))
+        calculated_time_label['text'] = str(Timing.average_time / Timing.average_time_count) + "s"
+
+
     gen_button = ttk.Button(frame, text="Generate", command=generate)
+    gen_button['state'] = 'disabled' #TODO: check
     gen_button.grid(column=2, row=1, sticky=tk.N, padx=default_padding, pady=default_padding)
 
     llp_button = ttk.Button(frame, text="Load last prompt", command=load_last_prompt)
@@ -283,9 +281,9 @@ if __name__ == "__main__":
     frame.pack()
     true_grid = []
 
-    for i in range(grid_size):
+    for i in range(grid_var.get()):
         row = []
-        for j in range(grid_size):
+        for j in range(grid_var.get()):
             row.append(0)
         true_grid.append(row)
 
