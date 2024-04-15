717917c070dd8ab9baaad996432339d96d2501a8
FallenLogic <68028253+FallenLogic@users.noreply.github.com>
2024-04-14 22:38:45 -0400

    Update language_processing.py

--- language_processing.py
+++ language_processing.py
@@ -1,7 +1,7 @@
 import training
 import os
 
-nouns = [x[0].split('\\')[-1] for x in os.walk("training")] #TODO see if it works for Capstone
+nouns = [x[0].split('\\')[-1] for x in os.walk("training")]  # TODO see if it works for Capstone
 print(nouns)
 position_words = ('in', 'on', 'by', 'next to')
 numbers = {}
@@ -27,24 +27,60 @@ with open("numbers.txt", 'r') as numfile:
         line_list = line.split(':')
         numbers[line_list[1].strip()] = line_list[0]
 
+
 def process_prompt(input_str, prop_x, prop_y, map_input):
     retrieved_props = []
-    wordlist = input_str.strip().split()
-    print(wordlist)
-    # TODO: get list of nouns too in addition to words
-    for i in range(len(wordlist) - 1):
-        word = wordlist[i]
-        next_word = wordlist[i + 1]
-        if word in nouns or word[:-1] in nouns:
-            if next_word in position_words:
-                print("{} {} ".format(word, next_word))
-                #TODO: position checks
+    input_word_list = input_str.strip().split()
+    print(input_word_list)
+    list_of_words = []
+
+    word1 = input_word_list[0]
+    if word1 in nouns:
+        list_of_words.append(word1)
+    if word1[:-1] in nouns:
+        list_of_words.append(word1)
+        
+    for i in range(0, len(input_word_list)-1):
+
+        word = input_word_list[i]
+        next_word = input_word_list[i+1]
+        print("WORDS:", word, next_word)
+        if word in nouns:
+            list_of_words.append(word)
+        if word[:-1] in nouns:
+            list_of_words.append(word)
         if word in numbers:
-            if next_word in nouns or next_word[:-1] in nouns: # checks for plural or potential typo (s at end)
-                print("we need {} of this prop: {}".format(numbers[wordlist[i]], wordlist[i + 1]))
+            if next_word in nouns:
                 for j in range(int(numbers[word])):
-                    return_list = training.load_props(next_word[:-1], prop_x, prop_y, map_input)
-                    for prop in return_list:
-                        retrieved_props.append(prop)
+                    list_of_words.append(word)
+            if next_word[:-1] in nouns:
+                for j in range(int(numbers[word])):
+                    list_of_words.append(next_word[:-1])
+
+    for item in list_of_words:
+        temp_list = training.load_props(item, prop_x, prop_y, map_input)
+        for item2 in temp_list:
+            retrieved_props.append(item2)
+
+    print("WORDLIST:", list_of_words)
+
+    # TODO: get list of nouns too in addition to words
+    # for i in range(len(wordlist) - 1):
+    #     word = wordlist[i]
+    #     next_word = wordlist[i + 1]
+    #     if word in numbers:
+    #         if next_word in nouns or next_word[:-1] in nouns:  # checks for plural or potential typo (s at end)
+    #             print("we need {} of this object: {}".format(numbers[wordlist[i]], wordlist[i + 1]))
+    #             for j in range(int(numbers[word])):
+    #                 return_list = training.load_props(next_word[:-1], prop_x, prop_y, map_input)
+    #                 for prop in return_list:
+    #                     retrieved_props.append(prop)
+    #     if word in nouns or word[:-1] in nouns:  # checks for plural
+    #         return_list = training.load_props(word[:-1], prop_x, prop_y, map_input)
+    #         for prop in return_list:
+    #             retrieved_props.append(prop)
+    #         if next_word in position_words:
+    #             print("{} {} ".format(word, next_word))
+    # TODO: position checks
 
     return retrieved_props
